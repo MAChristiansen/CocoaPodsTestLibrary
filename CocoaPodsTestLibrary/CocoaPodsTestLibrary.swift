dbd22cfcc71ef706422c533ca3b6b2a6555bd813
Marcus Christiansen <mac@trackman.com>
2020-04-29 08:21:20 +0200

    Initial Commit

---
+++ CocoaPodsTestLibrary/CocoaPodsTestLibrary.swift
@@ -0,0 +1,11 @@
+//
+//  CocoaPodsTestLibrary.swift
+//  CocoaPodsTestLibrary
+//
+//  Created by Marcus August Christiansen on 29/04/2020.
+//  Copyright © 2020 TrackMan. All rights reserved.
+//
+
+class CocoaPodsTestLibrary {
+
+}
