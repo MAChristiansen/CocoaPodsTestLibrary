a44cfc7641ab0de397ccf23c2ad9e162eb4b4cad
Marcus Christiansen <mac@trackman.com>
2020-04-29 09:00:06 +0200

    Initial Commit

---
+++ CocoaPodsTestLibrary/PrintController.swift
@@ -0,0 +1,18 @@
+//
+//  PrintController.swift
+//  CocoaPodsTestLibrary
+//
+//  Created by Marcus August Christiansen on 29/04/2020.
+//  Copyright © 2020 TrackMan. All rights reserved.
+//
+
+import Foundation
+
+struct PrintController {
+    
+    
+    func printHelloWorld() {
+        print("Hello World!!")
+    }
+    
+}
